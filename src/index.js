de772b474ac9e1feab912dbe17a61429bf98e061
Andriatiana Samyen Lee <Sam25v14@gmail.com>
2022-03-10 14:38:09 +0300

    graphql integration

--- src/index.js
+++ src/index.js
@@ -5,21 +5,38 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { ApolloProvider } from "react-apollo";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-boost";
 
 import { store, persistor } from "./redux/store";
 // import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
+const httpLink = createHttpLink({
+  uri: "https://crwn-clothing.com",
+});
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <PersistGate persistor={persistor}>
-          <App />
-        </PersistGate>
-      </BrowserRouter>
-    </React.StrictMode>
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
+        </BrowserRouter>
+      </React.StrictMode>
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );
 
